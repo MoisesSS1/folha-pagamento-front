7d84cff3b84347ff24b84146b3172a231d3efa6c
Moises <investmentmoises@gmail.com>
2023-06-24 02:31:14 -0300

    config icon page

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://sheet-back-c6170a37c530.herokuapp.com/",
+  withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
