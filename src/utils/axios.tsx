b3ef8739a457700971f5d88e48ae5b6a68c19e89
Moises <investmentmoises@gmail.com>
2023-06-23 22:55:06 -0300

    atualizando url para requisições

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://sheet-back-258f9a8b8603.herokuapp.com",
   withCredentials: false,
   headers: {
     "Access-Control-Allow-Origin": "*",
